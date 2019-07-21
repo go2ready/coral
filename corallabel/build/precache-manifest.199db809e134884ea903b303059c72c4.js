4b2c321f2c222d95d4e7206a2b2d8f3e37d235b5
Richard Wang <shw@microsoft.com>
2019-07-21 19:50:26 +0100

    Adding build

---
+++ corallabel/build/precache-manifest.199db809e134884ea903b303059c72c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097fed371e5b8babdedd3f518fd1d85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40ef314649fa30992b15",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "cd0689982eaf82c940a3",
+    "url": "/static/js/2.47df9263.chunk.js"
+  },
+  {
+    "revision": "40ef314649fa30992b15",
+    "url": "/static/js/main.40fd9871.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
